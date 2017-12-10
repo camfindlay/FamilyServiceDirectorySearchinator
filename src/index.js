337e3430bc0fe6e001278962bb29a5790f19d982
Brenda Wallace <brenda@wallace.net.nz>
2017-12-11 10:47:15 +1300

    Adding a router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Link } from 'react-router-dom'
 import axios from 'axios';
 import './index.css';
 
@@ -16,7 +17,42 @@ class SearchResult extends React.Component {
   }
 }
 
-class FamilyServicesSearch extends React.Component {
+class CategoryLink extends React.Component {
+  render() {
+    return (
+      <li className="category-link">
+        <Link to="whare" onClick={this.handleCategorySelection}>
+        {this.props.record.name}
+        </Link>
+      </li>
+      );
+  }
+}
+
+class Categories extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {categories: []};
+    // this.handleChange = this.handleChange.bind(this);
+    // this.handleSubmit = this.handleSubmit.bind(this);
+    this.fetchCategories();
+  }
+  fetchCategories() {
+    let sql = encodeURI('SELECT DISTINCT "LEVEL_1_CATEGORY" as name FROM "35de6bf8-b254-4025-89f5-da9eb6adf9a0" ORDER BY name')
+    let url = `https://catalogue.data.govt.nz/api/3/action/datastore_search_sql?sql=${sql}`;
+    axios.get(url)
+      .then(res => {
+        this.setState({ categories: res.data.result.records });
+    });
+  }
+  render() {
+    return this.state.categories.map((record) =>
+      <CategoryLink record={record} />
+    );
+  }
+}
+
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {value: '', results: []};
@@ -29,6 +65,9 @@ class FamilyServicesSearch extends React.Component {
   handleSubmit(event) {
     this.fetchResults(this.state.value);
     event.preventDefault();
+  }
+  handleCategorySelection(event) {
+    // debugger;
   }
   fetchResults(keyword) {
     axios.get(`https://catalogue.data.govt.nz/api/3/action/datastore_search?resource_id=35de6bf8-b254-4025-89f5-da9eb6adf9a0&q=${this.state.value}`)
@@ -52,12 +91,12 @@ class FamilyServicesSearch extends React.Component {
       </form>
       );
   }
-
   render() {
     return (
       <div>
         <h1>Whānau Services Search</h1>
         {this.renderSearchForm()}
+        <Categories />
         {this.renderSearchResults()}
       </div>
     );
@@ -65,8 +104,8 @@ class FamilyServicesSearch extends React.Component {
 }
 
 // ========================================
-
-ReactDOM.render(
-  <FamilyServicesSearch />,
-  document.getElementById('root')
-);
+ReactDOM.render((
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
+), document.getElementById('root'))
