445bf8bf14ff5460509eb30fe318cba41472d4d3
dlouise64 <dana.iti.work@gmail.com>
2018-02-07 20:16:53 +1300

    Cleanup code

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { NavItem } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 class FilterButton extends Component {
--- src/components/Service.js
+++ src/components/Service.js
@@ -7,18 +7,12 @@ class Service extends Component {
   }
   render() {
     const {
-      FSD_ID,
       PROVIDER_NAME,
       SERVICE_NAME,
       SERVICE_DETAIL,
-      ORGANISATION_PURPOSE,
       PUBLISHED_CONTACT_EMAIL_1,
       PUBLISHED_PHONE_1,
-      PROVIDER_CONTACT_AVAILABILITY,
-      PHYSICAL_ADDRESS,
-      DELIVERY_METHODS,
-      COST_TYPE,
-      SERVICE_REFERRALS
+      PROVIDER_CONTACT_AVAILABILITY
     } = this.props.record;
     return (
       <div className="service">
