66fe0602de744a883b3acc968f83767721589981
Dana Iti <dana.iti.work@gmail.com>
2018-02-08 17:57:58 +1300

    Updated the styles

--- src/components/Service.js
+++ src/components/Service.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import '../styles/SearchResults.css';
+import FaHand from 'react-icons/lib/fa/hand-o-up';
+import FaPhone from 'react-icons/lib/fa/phone';
+import FaMail from 'react-icons/lib/fa/envelope-o';
+import FaClock from 'react-icons/lib/fa/clock-o';
 
 class Service extends Component {
   constructor(props) {
@@ -18,23 +23,25 @@ class Service extends Component {
       .replace(/^-*/,'')
       .replace(/-*$/,'');
     }
-  } 
+  }
   render() {
     return (
       <div className="service">
+        <div className="search-result-hero">
         <Link to={{
         pathname: `/service/${this.urlify(this.props.record.PROVIDER_NAME)}`,
         state: this.props.record}} onClick={()=>this.props.changeResult(this.props.record.FSD_ID)}><h3>{this.props.record.PROVIDER_NAME}</h3></Link>
-
+        <p>{this.props.record.PHYSICAL_ADDRESS}</p>
+        <span><u>Show on map</u></span>
+        </div>
         <h4>{this.props.record.SERVICE_NAME}</h4>
         <p>{this.props.record.SERVICE_DETAIL}</p>
-        <dt>Email</dt>
-          <dd>{this.props.record.PUBLISHED_CONTACT_EMAIL_1}</dd>
-          <dt>Phone</dt>
-          <dd>
-            <a href={`tel:${this.props.record.PUBLISHED_PHONE_1}`}>{this.props.record.PUBLISHED_PHONE_1}</a>
-            <p><small><em>({this.props.record.PROVIDER_CONTACT_AVAILABILITY})</em></small></p>
-          </dd>
+        <ul className="list-stripped">
+          <li className="list-icon"><span><FaHand /></span> http://www.website.com</li>
+          <li className="list-icon"><span><FaPhone/></span> {this.props.record.PUBLISHED_PHONE_1}</li>
+          <li className="list-icon"><span><FaMail/></span> {this.props.record.PUBLISHED_CONTACT_EMAIL_1}</li>
+          <li className="list-icon"><span><FaClock/></span> {this.props.record.PROVIDER_CONTACT_AVAILABILITY}</li>
+        </ul>
       </div>
     );
   }
