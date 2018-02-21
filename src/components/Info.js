f93487fbbfc5270d4cf2f77f72cbbbb83142ca31
dlouise64 <dana.iti.work@gmail.com>
2018-02-21 21:19:29 +1300

    fix some errors with var naming

--- src/components/Info.js
+++ src/components/Info.js
@@ -3,30 +3,31 @@ import React, { Component } from 'react';
 class Info extends Component {
 
   render() {
-    const props = this.props.record.location.state;
+    if(this.props.record.location.state) {
     return (
-      <div className="container-fluid">
-        <div className="service-info">
-          <h2>{props.PROVIDER_NAME}</h2>
-          <h4>{props.SERVICE_NAME}</h4>
-          <p>{props.SERVICE_DETAIL}</p>
-          <dt>Email</dt>
-          <dd>{props.PUBLISHED_CONTACT_EMAIL_1}</dd>
-          <dt>Phone</dt>
-          <dd>
-            <a className="telephone" href={`tel:${props.PUBLISHED_PHONE_1}`}>{props.PUBLISHED_PHONE_1}</a>
-            <p><small><em>({props.PROVIDER_CONTACT_AVAILABILITY})</em></small></p>
-          </dd>
-          <dl>
-            <dt>Organisation purpose</dt><dd>{props.ORGANISATION_PURPOSE}</dd>
-            <dt>Physical address</dt><dd>{props.PHYSICAL_ADDRESS}</dd>
-            <dt>Delivery Methods</dt><dd>{props.DELIVERY_METHODS}</dd>
-            <dt>Costs</dt><dd>{props.COST_TYPE}</dd>
-            <dt>Service referrals</dt><dd>{props.SERVICE_REFERRALS}</dd>
-          </dl>
+        <div className="container-fluid">
+          <div className="service-info">
+            <h2>{this.props.record.location.state.PROVIDER_NAME}</h2>
+            <h4>{this.props.record.location.state.SERVICE_NAME}</h4>
+            <p>{this.props.record.location.state.SERVICE_DETAIL}</p>
+            <dt>Email</dt>
+            <dd>{this.props.record.location.state.PUBLISHED_CONTACT_EMAIL_1}</dd>
+            <dt>Phone</dt>
+            <dd>
+              <a className="telephone" href={`tel:${this.props.record.location.state.PUBLISHED_PHONE_1}`}>{this.props.record.location.state.PUBLISHED_PHONE_1}</a>
+              <p><small><em>({this.props.record.location.state.PROVIDER_CONTACT_AVAILABILITY})</em></small></p>
+            </dd>
+            <dl>
+              <dt>Organisation purpose</dt><dd>{this.props.record.location.state.ORGANISATION_PURPOSE}</dd>
+              <dt>Physical address</dt><dd>{this.props.record.location.state.PHYSICAL_ADDRESS}</dd>
+              <dt>Delivery Methods</dt><dd>{this.props.record.location.state.DELIVERY_METHODS}</dd>
+              <dt>Costs</dt><dd>{this.props.record.location.state.COST_TYPE}</dd>
+              <dt>Service referrals</dt><dd>{this.props.record.location.state.SERVICE_REFERRALS}</dd>
+            </dl>
+          </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
--- src/components/ServiceMapMarker.js
+++ src/components/ServiceMapMarker.js
@@ -6,20 +6,18 @@ class ServiceMapMarker extends Component {
     super(props);
     this.state = {};
   }
+
   render() {
-    const props = this.props.record;
-
-    if (props.LATITUDE && props.LONGITUDE) {
-      let position = [props.LATITUDE, props.LONGITUDE];
-
+    if (this.props.record.LATITUDE && this.props.record.LONGITUDE) {
+      let position = [this.props.record.LATITUDE, this.props.record.LONGITUDE];
       return (
         <Marker position={position}>
           <Popup>
             <span>
-              <h5>{props.PROVIDER_NAME}</h5>
-              <h6>{props.SERVICE_NAME}</h6>
-              <p>{props.ORGANISATION_PURPOSE}</p>
-              <p>{props.PHYSICAL_ADDRESS}</p>
+              <h5>{this.props.record.PROVIDER_NAME}</h5>
+              <h6>{this.props.record.SERVICE_NAME}</h6>
+              <p>{this.props.record.ORGANISATION_PURPOSE}</p>
+              <p>{this.props.record.PHYSICAL_ADDRESS}</p>
             </span>
           </Popup>
         </Marker>
