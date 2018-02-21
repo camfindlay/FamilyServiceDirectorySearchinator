db0c98c40c9f8b60004776a06f65385310fb4e36
Brenda Wallace <shiny@cpan.org>
2018-02-22 09:57:50 +1300

    Updates

--- src/__tests__/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import App from '../components/App';
-import { shallow } from 'enzyme';
-
-it('shows the correct h1 value', () => {
-  const wrapper = shallow(<App />);
-  expect(wrapper.find('h1').length).toBe(1);
-  expect(wrapper.find('h1').at(0).text()).toBe('Find Whānau Services in your area');
-});
