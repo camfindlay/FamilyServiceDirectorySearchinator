91c84e81db31ed8c9b1a0a2cd0e6613346a97d04
dlouise64 <dana.iti.work@gmail.com>
2018-02-28 01:03:41 +1300

    converted to stateless components

--- src/components/Map/ServiceMapMarker.js
+++ src/components/Map/ServiceMapMarker.js
@@ -1,31 +1,24 @@
 import React, { Component } from 'react';
 import { Marker, Popup } from 'react-leaflet';
 
-class ServiceMapMarker extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    if (this.props.record.LATITUDE && this.props.record.LONGITUDE) {
-      let position = [this.props.record.LATITUDE, this.props.record.LONGITUDE];
-      return (
+const ServiceMapMarker = ( props ) => {
+  let position = [props.record.LATITUDE, props.record.LONGITUDE];
+  return (
+    <div>
+      {props.record.LATITUDE && props.record.LONGITUDE &&
         <Marker position={position}>
           <Popup>
             <span>
-              <h5>{this.props.record.PROVIDER_NAME}</h5>
-              <h6>{this.props.record.SERVICE_NAME}</h6>
-              <p>{this.props.record.ORGANISATION_PURPOSE}</p>
-              <p>{this.props.record.PHYSICAL_ADDRESS}</p>
+              <h5>{props.record.PROVIDER_NAME}</h5>
+              <h6>{props.record.SERVICE_NAME}</h6>
+              <p>{props.record.ORGANISATION_PURPOSE}</p>
+              <p>{props.record.PHYSICAL_ADDRESS}</p>
             </span>
           </Popup>
         </Marker>
-      );
-    } else {
-      return '';
-    }
-  }
-}
+      }
+    </div>
+  )
+};
 
 export default ServiceMapMarker;
--- src/components/Service/Filters.js
+++ src/components/Service/Filters.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
-class Filters extends React.Component {
-  render() {
-    return (
-      <nav className="nav">
-        {this.props.data.filters.map(data => {
-          return (<button className={this.props.data.category === data.name ? 'selected'  : ''} key={data.num} 
-          onClick={()=> {this.props.data.loadResults(data.name, '');}}>{data.name}</button>);
-        })}
-      </nav>
-    );
-  }
+const Filters = ( props ) => {
+  return (
+    <nav className="nav">
+      {props.data.filters.map(data => {
+        return (<button className={props.data.category === data.name ? 'selected'  : ''} key={data.num} 
+        onClick={()=> {props.data.loadResults(data.name, '');}}>{data.name}</button>);
+      })}
+    </nav>
+  );
 }
 
 export default Filters;
