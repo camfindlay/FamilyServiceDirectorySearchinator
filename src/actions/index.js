5be985ed4915c17b541950442b0d9bbe0e2cdd1d
dlouise64 <dana.iti.work@gmail.com>
2018-02-28 11:12:24 +1300

    refactor API calls

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,11 +1,14 @@
 import axios from 'axios';
+const RESOURCE_ID = process.env.REACT_APP_API_RESOURCE_ID;
+const API_PATH = `${process.env.REACT_APP_API_PATH}`;
 
-const resourceId = process.env.REACT_APP_API_RESOURCE_ID;
-const root_url = `${process.env.REACT_APP_API}`;
+let filters = (category) => category ? `{"LEVEL_1_CATEGORY":"${category}"}` : '';
+let q = (keyword) => keyword.length > 2 ? `${keyword}` : '';
+const fields = () => 'LEVEL_1_CATEGORY,FSD_ID,LONGITUDE,LATITUDE,PROVIDER_NAME,PUBLISHED_CONTACT_EMAIL_1,PUBLISHED_PHONE_1,PROVIDER_CONTACT_AVAILABILITY,ORGANISATION_PURPOSE,PHYSICAL_ADDRESS,SERVICE_NAME,SERVICE_DETAIL,DELIVERY_METHODS,COST_TYPE,SERVICE_REFERRALS';
 
 export function loadFilters(){
-  let sql = encodeURI(`SELECT "LEVEL_1_CATEGORY" as name, COUNT(*) as num FROM "${resourceId}" GROUP BY name ORDER BY name`);
-  let url = `${root_url}?sql=${sql}`;
+  let sql = encodeURI(`SELECT "LEVEL_1_CATEGORY" as name, COUNT(*) as num FROM "${RESOURCE_ID}" GROUP BY name ORDER BY name`);
+  let url = `${API_PATH}datastore_search_sql?sql=${sql}`;
   return (dispatch) => {
     return axios.get(url).then((response)=>{
       dispatch(showFilters(response.data.result.records));
@@ -13,26 +16,10 @@ export function loadFilters(){
   };
 }
 
-const fields = ()=> {
-  return 'LEVEL_1_CATEGORY,FSD_ID,LONGITUDE,LATITUDE,PROVIDER_NAME,PUBLISHED_CONTACT_EMAIL_1,PUBLISHED_PHONE_1,PROVIDER_CONTACT_AVAILABILITY,ORGANISATION_PURPOSE,PHYSICAL_ADDRESS,SERVICE_NAME,SERVICE_DETAIL,DELIVERY_METHODS,COST_TYPE,SERVICE_REFERRALS';
-};
-
 export function loadResults(category, keyword) {
-  
-  let url = `${root_url}?resource_id=${resourceId}&fields=${fields()}`;
-  var query;
-  if (keyword.length > 2) {
-    query = `&q=${keyword}}&distinct=true`;
-  } else if(category) {
-    query = `&q=&distinct=true&filters={"LEVEL_1_CATEGORY":"${category}"}`; 
-  } else if(category && keyword.length > 2) {
-    query = `&q=${keyword}&distinct=true&filters={"LEVEL_1_CATEGORY":"${category}"}`; 
-  } else {
-    query = `&fields=${fields()}&distinct=true`; 
-  }
-
+  let url = encodeURI(`${API_PATH}datastore_search?resource_id=${RESOURCE_ID}&fields=${fields()}&q=${q(keyword)}&distinct=true&filters=${filters(category)}`);
   return (dispatch) => {
-    return axios.get(`${url}${query}`).then((response)=>{
+    return axios.get(url).then((response)=>{
       dispatch(showResults(response.data.result.records, category, keyword));
     });
   };
@@ -47,14 +34,14 @@ export function fetchAddressFinder(address, pxid) {
     return axios.get(`${url}${query}`).then((response)=>{
       dispatch(fetchAddresses(response.data.completions));
     });
-  };
+  }
 }
 
 export function showFilters(filters){
   return {
     type: 'SHOW_FILTERS',
     filters
-  };
+  }
 }
 
 export function showResults(results, category, keyword) {
@@ -63,11 +50,11 @@ export function showResults(results, category, keyword) {
     results,
     category,
     keyword
-  };
+  }
 }
 export function fetchAddresses(addresses) {
   return {
     type: 'FETCH_ADDRESSES',
     addresses
-  };
+  }
 }
