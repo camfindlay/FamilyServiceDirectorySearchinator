b28d8b0c6de386f87d868eeae848152ee0affa8e
Hamish Fraser <hamish@verb.co.nz>
2018-04-04 12:27:50 +1200

    mobile category select bug fix

--- src/components/Service/Filters.js
+++ src/components/Service/Filters.js
@@ -22,8 +22,7 @@ class Filters extends React.Component {
     this.categoryChange(this.props.searchVars.category === event.target.value ? '' : event.target.value);
   }
 
-  categoryChange(history,category){
-    history.push((this.props.searchVars.category === category ? '' : '/category/'+encodeURIComponent(category)));
+  categoryChange(category){
     const clone = {...this.props.searchVars};
     clone.category = category;
     this.props.loadResults(clone);
@@ -37,22 +36,21 @@ class Filters extends React.Component {
             return (<Route key={index} render={({ history}) => (
               <button className={this.props.searchVars.category === data.name ? 'selected'  : ''} key={data.num}
                 onClick={()=> {
-                  this.categoryChange(history,(this.props.searchVars.category === data.name ? '' : data.name));
-                }}>
-                {data.name}
-              </button>)} />
+                  history.push((this.props.searchVars.category === data.name ? '' : '/category/'+encodeURIComponent(data.name)));
+                  this.categoryChange((this.props.searchVars.category === data.name ? '' : data.name));
+                }}>{data.name}</button>)} />
             );
           })}
         </nav>
         <Route render={({history}) => (
           <select value={this.props.searchVars.category} onChange={(event)=> {
-            this.categoryChange(history,this.props.searchVars.category === event.target.value ? '' : event.target.value);
+            history.push((this.props.searchVars.category === event.target.value ? '' : '/category/'+encodeURIComponent(event.target.value)));
+            this.categoryChange(this.props.searchVars.category === event.target.value ? '' : event.target.value);
           }}>
             <option name="filters" defaultValue>-- Select Category --</option>
             {this.props.filters.map(data => {
               return (<option key={data.num} name="filters">{data.name}</option>);
-            })}
-          </select>)} />
+            })}</select>)} />
       </div>
     );
   }
