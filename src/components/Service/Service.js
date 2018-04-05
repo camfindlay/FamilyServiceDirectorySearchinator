4acc1b2b519d762975409efb8dc3ed4a5964bf56
Hamish Fraser <hamish@verb.co.nz>
2018-04-05 22:10:13 +1200

    distance away from address, proximity margins to assist iphone users

--- src/components/Service/Service.js
+++ src/components/Service/Service.js
@@ -11,7 +11,8 @@ class Service extends Component {
       <div>
         <div className="service">
           <div className="search-result-hero">
-            <Link to={`/service/${this.props.results.FSD_ID}${this.props.searchVars.category ? '/'+encodeURIComponent(this.props.searchVars.category) : ''}`}><h2>{this.props.results.PROVIDER_NAME}</h2></Link>
+            <h2><Link to={`/service/${this.props.results.FSD_ID}${this.props.searchVars.category ? '/'+encodeURIComponent(this.props.searchVars.category) : ''}`}>{this.props.results.PROVIDER_NAME}</Link></h2>
+            {this.props.results.DISTANCE && <small>{((this.props.results.DISTANCE*1)/1000).toFixed(1)} km</small>}
             <ServiceContactDetail phone={this.props.results.PUBLISHED_PHONE_1} email={this.props.results.PUBLISHED_CONTACT_EMAIL_1} hours={this.props.results.PROVIDER_CONTACT_AVAILABILITY} />
           </div>
           <ServiceDetail results={this.props.results} changeCategory={this.props.changeCategory} searchVars={this.props.searchVars} serviceId={this.props.serviceId} loadimmediately={false} loadResults={this.props.loadResults} />
