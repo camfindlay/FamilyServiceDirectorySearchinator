132958ef2febc5293821013c2ce5ff33697db5b2
Brenda Wallace <shiny@cpan.org>
2018-05-31 13:25:21 +1200

    upgrading npm packages

--- src/index.js
+++ src/index.js
@@ -4,15 +4,12 @@ import './styles/index.css';
 import AppCon from './container/app-container';
 import ServiceInfo from './components/Service/ServiceInfo';
 import Footer from './components/Templates/Footer';
-// import { createStore, applyMiddleware, compose } from 'redux';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 import reducers from './reducers/index';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 const store = createStore(reducers, applyMiddleware(thunk));
 
 class App extends React.Component {
